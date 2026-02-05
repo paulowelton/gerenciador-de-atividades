2c4e23b0a055689b34a27b58ebea630fba053853
paulowelton <paulinho.welton08@gmail.com>
2026-02-05 19:34:49 -0300

    adicioando cors e fazendo uma requisicao teste no frontend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from src.routes.tasks import router as tasks_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from "react"
+
 function App(){
-  const message = 'Hello Word!'
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/tasks/")
+    .then(response => response.json())
+    .then(data => {
+      setTasks(data)
+    })
+  })
 
   return (
-    <div>{message}</div>
+    <div>
+      {tasks.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
+    </div>
   )
 }
 
