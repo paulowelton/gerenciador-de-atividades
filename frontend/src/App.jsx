ee46e9aafb31ca43bed8dcd67409a4e6a1b078bb
paulowelton <paulinho.welton08@gmail.com>
2026-02-05 19:57:58 -0300

    criado component de listar tarefas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
 import { useState, useEffect } from "react"
+import ListTasks from "./components/ListTasks";
 
 function App(){
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/tasks/")
-    .then(response => response.json())
-    .then(data => {
-      setTasks(data)
-    })
+    function loadTasks() {
+      fetch("http://127.0.0.1:8000/tasks/")
+      .then(response => response.json())
+      .then(data => {
+        setTasks(data)
+      })
+    }
+
+    loadTasks()
   })
 
   return (
-    <div>
-      {tasks.map((task) => (
-        <p key={task.id}>{task.title}</p>
-      ))}
+    <div className="w-screen h-screen bg-slate-100 flex justify-center items-center">
+      <ListTasks tasks={tasks}/>
     </div>
   )
 }
